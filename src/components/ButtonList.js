13c83423eb739664109bb416e8340556ba183729
AkashKinkarPandey <akashkinkarpandey@gmail.com>
2023-04-28 17:36:05 +0530

    nested comments done

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -4,7 +4,7 @@ import { Button } from './Button'
 const list=["Streaming","All","Live","Game","Cricket","Soccer","Cooking","Badminton"]
 const ButtonList = () => {
   return (
-   <div className='flex'>
+   <div className='flex '>
     {
       list.map((item,index)=>(
         <Button key={item} name={item}/>
--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux'
 import { closeMenu } from '../utils/appSlice';
 import { useParams } from 'react-router-dom';
 import { useSearchParams } from 'react-router-dom';
+import CommentsContainer from './CommentsContainer';
 const WatchPage = () => {
   const [searchParams] = useSearchParams();
   console.log(searchParams);
@@ -12,8 +13,11 @@ const WatchPage = () => {
     dispatch(closeMenu());
   },[])
   return (
-    <div lassName="px-5">
+    <div className='flex flex-col'>    
+    <div className="px-5">
       <iframe width="800" height="400" src={"https://www.youtube.com/embed/"+searchParams.get("v")}title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+    </div>
+    <CommentsContainer/>
     </div>
   )
 }
