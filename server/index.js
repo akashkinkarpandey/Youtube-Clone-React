3bd482425fb9750867f066351bed14e9526a3cc2
Akash Kinkar Pandey <akashkinkarpandey@gmail.com>
2025-09-10 00:06:07 +0530

    Security Mechanisms in Backend Introduced

--- server/index.js
+++ server/index.js
@@ -1,15 +1,48 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import hpp from "hpp";
+import sanitizer from "perfect-express-sanitizer";
+import { rateLimit } from "express-rate-limit";
+import helmet from "helmet";
+
 dotenv.config();
+const PORT = process.env.PORT || 5000;
 
 const app = express();
-const PORT = 5000;
+app.use(helmet());
+app.use(hpp());
+const limiter = rateLimit({
+  windowMs: 8 * 60 * 1000, // 8 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  // store: ... , // Redis, Memcached, etc. See below.
+});
+app.use(limiter);
+app.use(
+  sanitizer.clean({
+    xss: true,
+    noSql: true,
+    sql: true,
+  })
+);
 const API_KEY = process.env.GOOGLE_API_KEY;
 
 app.use(
   cors({
     origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "X-Requested-With",
+      "device-remember-token",
+      "Access-Control-Allow-Origin",
+      "Origin",
+      "Accept",
+    ],
   })
 );
 
